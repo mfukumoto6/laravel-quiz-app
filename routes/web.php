fb863d8af0fe2c2a9e33b7d161fa4b5036d15447
Masaki Fukumoto <fm8624@gmail.com>
2023-06-01 22:42:47 +0900

    コントローラとルートをリファクタリング

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\QuizController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,20 +18,4 @@ Route::get('/', function () {
   return view('welcome');
 });
 
-Route::get('quizzes', function () {
-  return view('quizzes.index');
-})->name('quizzes.index');
-
-Route::get('quizzes/show', function () {
-  return view('quizzes.show');
-})->name('quizzes.show');
-
-Route::get('quizzes/create', function () {
-  return view('quizzes.create');
-})->name('quizzes.create');
-
-Route::delete('quizzes/{id}', 'App\Http\Controllers\QuizController@destroy')->name('quizzes.delete');
-
-// POSTのリクエストが来た時にどういったコントローラを呼ぶか、ここではstoreメソッド
-// POSTメソッドで'quizzes'のURLにリクエストが来たときに、自動的にQuizControllerのstoreメソッドを呼ぶ
-Route::post('quizzes', 'App\Http\Controllers\QuizController@store')->name('quizzes.store');
+Route::resource('quizzes', QuizController::class);
